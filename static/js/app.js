c3248b2b072d295ad69d503475f85d76a362de3f
patelavani332 <patelavani332@gmail.com>
2023-04-05 12:27:14 -0400

    Updating the code

---
+++ static/js/app.js
@@ -0,0 +1,102 @@
+// Creating bubble chart and bar chart for the samples 
+
+function buildCharts(sample){
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+        let samples = data.samples;
+        let resultArray = samples.filter((sampleDictionary) => sampleDictionary.id == sample);
+        let result = resultArray[0];
+
+        let otuIDs = result.otu_ids;
+        let otuLabels = result.otu_labels;
+        let sampleValues = result.sample_values;
+     
+    // Create bubble chart for bacteria cultures per sample. (OTU ID vs sample values)
+        let bubbleLayout = {
+            title : "<b>Bacteria Cultures Per Sample</b>",
+            margin : { t: 0},
+            hovermode : "closest",
+            xaxis : {title: "OTU ID"},
+            margin : { t: 30}
+        }
+        let bubbleData = [
+            {
+                x : otuIDs,
+                y: sampleValues,
+                text : otuLabels,
+                mode : "markers",
+                marker: {
+                    size: sampleValues,
+                    color: otuIDs,
+                    colorscale: "Earth"
+                }
+            }
+        ]
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout)
+
+    // Create horizontal bar chart for top 10 bacteria cultures found in individual 
+        let yticks = otuIDs.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        let barData = [
+            {
+               y: yticks,
+               x: sampleValues.slice(0, 10).reverse(),
+               text: otuLabels.slice(0, 10).reverse(),
+               type: "bar",
+               orientation: "h"
+
+            }
+        ]
+
+        let barLayout = {
+            title: "<b>Top 10 Bacteria Cultures Found</b>",
+            margine: { t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barData, barLayout)
+
+    });
+}
+
+// Displaying individual's demographic information (sample metadata)
+function buildMetadata(sample){
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+        let metadata = data.metadata;
+
+        let resultArray = metadata.filter(sampleDictionary => sampleDictionary.id == sample);
+
+        let result = resultArray[0];
+
+        let PANEL = d3.select("#sample-metadata");
+        PANEL.html("");
+
+        for (key in result) {
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${result[key]}`)
+        }
+
+        buildGauge(result.wfreq)
+    })
+}
+
+
+function init(){
+    let selector = d3.select("#selDataset");
+
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+        let sampleNames = data.names;
+        for(let i = 0; i < sampleNames.length; i++){
+            selector.append("option").text(sampleNames[i]).property("value", sampleNames[i]);
+        }
+
+        let firstSample = sampleNames[0];
+        buildCharts(firstSample);
+        buildMetadata(firstSample);
+    })
+
+}
+
+function optionChanged(newSample){
+    buildCharts(newSample);
+    buildMetadata(newSample);
+}
+
+init();
